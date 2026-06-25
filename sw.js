e75f087c299d6406f1b8b10fa46f71b942c8502a
sandrexis2323-cmyk <sandrexis2323@gmail.com>
2026-06-25 13:40:14 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+var CACHE = 'mnq-risk-v1';
+var FILES = [
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).catch(function() {
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
